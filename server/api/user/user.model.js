1578c9b4ee7270e21da24293b68d079e7dcc2bb5
Ben Halverson <benhalverson33@gmail.com>
2015-07-11 19:00:03 -0700

    updated nav bar and search.html added footer

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -6,10 +6,10 @@ var crypto = require('crypto');
 var authTypes = ['github', 'twitter', 'facebook', 'google'];
 
 var UserSchema = new Schema({
-  firstName: { type: String, lowercase: true, required: true },
-  LastName: { type: String, lowercase: true, required: true },
+  firstName: { type: String, lowercase: true },
+  LastName: { type: String, lowercase: true },
   email: { type: String, lowercase: true, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: Number },
   city: String,
   state: { type: String, uppercase: true },
   zipcode: Number,
